56368a7a0822458beed2d8ed7ab774d0adc4bb44
intelaashish <aashish73134@gmail.com>
2020-04-08 11:54:03 -0700

    Initial Commit

---
+++ Introduction_to_Haskell/Values_and_Basic_Types.hs
@@ -0,0 +1,24 @@
+
+
+
+--Assign 5 as an integer
+5:: Int
+
+--Assign 5 as a floating point
+5::Float
+
+--Create a char H
+"H"
+
+--Create a list of chars --> A string
+"Hello"
+
+--Basic Arithmetic
+3 * 4.0
+
+--Comparison
+3 < 4
+
+--if-else statement
+if 3<4 then 1 else 0 --returns 1
+if True then 1 else 0
